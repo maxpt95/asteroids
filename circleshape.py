90501a49af2935629ea3b83a050b1b937557360a
Maximiliano Perez Tambasco <maximiliano.pereztambasco@axpo.com>
2025-10-31 08:42:04 +0100

    Add type hints

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,4 @@
-from pygame import Vector2
+from pygame import Vector2, Surface
 from pygame.sprite import Sprite
 
 
@@ -6,9 +6,9 @@ class CircleShape(Sprite):
     """A base class for circular game objects.
 
     Attributes:
-        - x (float): position in the x axis.
-        - y (float): position in the y axis.
-        - radius (float): distance between the center of the circle to its border.
+        x (float): position in the x axis.
+        y (float): position in the y axis.
+        radius (float): distance between the center of the circle to its border.
     """
 
     def __init__(self, x: float, y: float, radius: float):
@@ -22,10 +22,10 @@ class CircleShape(Sprite):
         self.velocity = Vector2(0, 0)
         self.radius = radius
 
-    def draw(self, screen):
+    def draw(self, screen: Surface):
         # sub-classes must override
         pass
 
-    def update(self, dt):
+    def update(self, dt: float):
         # sub-classes must override
         pass
