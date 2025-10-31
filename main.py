5d7e0e2fe5b177a868a7f8b3c1670d2cbadf9519
Maximiliano Perez Tambasco <maximiliano.pereztambasco@axpo.com>
2025-10-31 08:11:36 +0100

    Extract game loop from main, and add delta time

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import pygame
-from pygame import Color
+from pygame import Surface
+from pygame.time import Clock
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+def run_game_loop(screen: Surface, clock: Clock):
+    delta_time = 0
+    # game loop
+    while True:
+        # check for inputs
+        for event in pygame.event.get():
+            # update the game world
+            if event.type == pygame.QUIT:
+                return
+
+        # draw the game in the screen
+        screen.fill("black")
+        pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
     print("Starting Asteroids!")
 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-
-        screen.fill("black")
-        pygame.display.flip()
+    run_game_loop(screen, clock)
 
 
 if __name__ == "__main__":
