9d910e62901ab314711d099f6371712aecc9081e
Maximiliano Perez Tambasco <maximiliano.pereztambasco@axpo.com>
2025-10-31 08:59:20 +0100

    Add player to game

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import pygame
 from pygame import Surface
 from pygame.time import Clock
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
-def run_game_loop(screen: Surface, clock: Clock):
-    delta_time = 0
-    # game loop
-    while True:
-        # check for inputs
-        for event in pygame.event.get():
-            # update the game world
-            if event.type == pygame.QUIT:
-                return
+class Game:
+    def __init__(self, player: Player):
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.player = player
 
-        # draw the game in the screen
-        screen.fill("black")
-        pygame.display.flip()
-        delta_time = clock.tick(60) / 1000
+    def run(self):
+        delta_time = 0
+        # game loop
+        while True:
+            # check for inputs
+            for event in pygame.event.get():
+                # update the game world
+                if event.type == pygame.QUIT:
+                    return
+
+            # draw the game in the screen
+            self.screen.fill("black")
+            self.player.draw(self.screen)
+            pygame.display.flip()
+            delta_time = self.clock.tick(60) / 1000
 
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-
     print("Starting Asteroids!")
 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode()
 
-    run_game_loop(screen, clock)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = Game(player)
+    asteroids.run()
 
 
 if __name__ == "__main__":
