22c3e464e464e730238e4bfceb6629bdd9395660
maxpt95 <m.perez.tambasco@gmail.com>
2025-11-08 17:14:34 +0100

    Move Game class logic into main() to enable logging

--- main.py
+++ main.py
@@ -1,69 +1,14 @@
 import sys
-from typing import NamedTuple
+
 
 import pygame
-from pygame.sprite import Group
+
 from pygame.time import Clock
 
 from asteroid_field import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from shapes import Asteroid, Player, Shot
 from logger import log_state
-
-
-class SpriteGroups(NamedTuple):
-    """Container for game sprite groups."""
-
-    asteroids: Group = Group()
-    drawables: Group = Group()
-    shots: Group = Group()
-    updatables: Group = Group()
-
-
-class Game:
-    def __init__(self, player: Player, groups: SpriteGroups):
-        self.clock = Clock()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.groups = groups
-        self.player = player
-
-    def draw(self):
-        """Draw the game on the screen."""
-        self.screen.fill("black")
-
-        for obj in self.groups.drawables:
-            obj.draw(self.screen)
-
-        pygame.display.flip()
-
-    def is_game_over(self):
-        """True if any game over condition is met."""
-        for ast in self.groups.asteroids:
-            if ast.collides_with(self.player):
-                return True
-
-        return False
-
-    def run(self):
-        """Run the game."""
-        _ = AsteroidField()
-        delta_time = 0
-        # game loop
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-
-            self.groups.updatables.update(delta_time)
-
-            if self.is_game_over():
-                print("Game over!")
-                sys.exit()
-
-            self.draw()
-
-            delta_time = self.clock.tick(60) / 1000
-            log_state()
 
 
 def main():
@@ -73,18 +18,45 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    groups = SpriteGroups()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Player.containers = (groups.updatables, groups.drawables)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    Shot.containers = (groups.shots, groups.updatables, groups.drawables)
+    Shot.containers = (shots, updatable, drawable)
 
-    Asteroid.containers = (groups.asteroids, groups.updatables, groups.drawables)
-    AsteroidField.containers = (groups.updatables,)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
 
-    game = Game(player, groups)
-    game.run()
+    clock = Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    delta_time = 0
+    # game loop
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(delta_time)
+
+        for ast in asteroids:
+            if ast.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
